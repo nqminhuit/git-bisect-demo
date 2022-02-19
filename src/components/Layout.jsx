8f958e5012f9f22683106172a81fddb5808b6945
Minh Nguyen <nqminhuit@gmail.com>
2022-02-19 14:06:29 +0700

    1: function to sort string numbers

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 export default function Layout() {
 
   const [randomStrings, setRandomStrings] = useState([]);
+  const [sortByNumberString, setSortByNumberString] = useState([]);
 
   const fromArray = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
     "zero", "hello", "hi", "good", "afternoon", "git", "bisect", "demonstration", "central", "management",
@@ -15,6 +16,10 @@ export default function Layout() {
         <input type="button" className="btn btn-primary" value="Random String" onClick={() => generateRandomString()} />
         <span className="ms-3">{randomStrings.map(str => str + ", ")}</span>
       </div>
+      <div className="mt-3">
+        <input type="button" className="btn btn-primary" value="Sort Number String" onClick={() => sortStringNumber(randomStrings)} />
+        <span className="ms-3">{sortByNumberString.map(str => str + ", ")}</span>
+      </div>
     </div>
   );
 
@@ -25,6 +30,51 @@ export default function Layout() {
       arr.push(fromArray[index]);
     }
     setRandomStrings(arr);
+  }
+
+  function sortStringNumber(strArr = []) {
+    if (strArr.length < 1) {
+      return;
+    }
+    const res = strArr.map(convertStringToNumber)
+      .filter(i => i !== undefined)
+      .sort((a, b) => a - b)
+      .map(convertNumberToString);
+    setSortByNumberString(res);
+  }
+
+  function convertStringToNumber(str = "") {
+    switch (str) {
+      case "zero": return 0;
+      case "one": return 1;
+      case "two": return 2;
+      case "three": return 3;
+      case "four": return 4;
+      case "five": return 5;
+      case "six": return 6;
+      case "seven": return 7;
+      case "eight": return 8;
+      case "nine": return 9;
+      case "ten": return 10;
+      default: break;
+    }
+  }
+
+  function convertNumberToString(str = -1) {
+    switch (str) {
+      case 0: return "zero";
+      case 1: return "one";
+      case 2: return "two";
+      case 3: return "three";
+      case 4: return "four";
+      case 5: return "five";
+      case 6: return "six";
+      case 7: return "seven";
+      case 8: return "eight";
+      case 9: return "nine";
+      case 10: return "ten";
+      default: break;
+    }
   }
 
   function getRandomInt(min, max) {
