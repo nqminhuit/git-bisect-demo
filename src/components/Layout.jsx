ce5bb0e43cf44df6dcf55072aecd277cf2474ebd
Minh Nguyen <nqminhuit@gmail.com>
2022-02-19 12:46:00 +0700

    1: add layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,6 +5,7 @@ export default function Layout() {
   return (
     <div className="container mt-3">
       <h3>This is git bisect demo</h3>
+      <input type="button" className="btn btn-primary" value="Random String" />
     </div>
   );
 
