0ee3dfe37d7065dd1da8438483426b100167859a
Minh Nguyen <nqminhuit@gmail.com>
2022-02-19 14:50:30 +0700

    2: add function to checkboxes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { convertNumberToString, convertStringToNumber, getRandomInt } from "../utils/NumbersUtils.js";
 
 export default function Layout() {
@@ -6,6 +6,16 @@ export default function Layout() {
   const [randomStrings, setRandomStrings] = useState([]);
   const [sortByNumberString, setSortByNumberString] = useState([]);
   const [sortString, setSortString] = useState([]);
+  const [checkedStringNumber, setCheckedStringNumber] = useState(false);
+  const [checkedString, setCheckedString] = useState(false);
+
+  useEffect(() => {
+    sortStrings(checkedString ? randomStrings : []);
+  }, [checkedString]);
+
+  useEffect(() => {
+    sortStringNumber(checkedStringNumber ? randomStrings : []);
+  }, [checkedStringNumber]);
 
   const fromArray = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
     "zero", "hello", "hi", "good", "afternoon", "git", "bisect", "demonstration", "central", "management",
@@ -22,16 +32,16 @@ export default function Layout() {
 
       <div className="mt-3">
         <div className="form-check">
-          <input type="checkbox" className="form-check-input" id="checkedSortNumber" onClick={() => sortStringNumber(randomStrings)} />
-          <label className="form-check-label" for="checkedSortNumber">Sort by string numbers:</label>
+          <input type="checkbox" className="form-check-input" id="checkedSortNumber" onClick={e => setCheckedStringNumber(e.target.checked)} />
+          <label className="form-check-label" htmlFor="checkedSortNumber">Sort by string numbers:</label>
           <span className="ms-3">{sortByNumberString.map(str => " " + str).toString().trim()}</span>
         </div>
       </div>
 
       <div className="mt-2">
         <div className="form-check">
-          <input type="checkbox" className="form-check-input" id="checkedSortString" onClick={() => sortStrings(randomStrings)} />
-          <label className="form-check-label">Sort by strings:</label>
+          <input type="checkbox" className="form-check-input" id="checkedSortString" onClick={e => setCheckedString(e.target.checked)} />
+          <label className="form-check-label" htmlFor="checkedSortString">Sort by strings:</label>
           <span className="ms-3">{sortString.map(str => " " + str).toString().trim()}</span>
         </div>
       </div>
@@ -48,10 +58,6 @@ export default function Layout() {
   }
 
   function sortStringNumber(strArr = []) {
-    if (strArr.length < 1) {
-      return;
-    }
-
     const res = strArr.map(convertStringToNumber)
       .filter(i => i !== undefined)
       .sort((a, b) => a + b)
@@ -61,10 +67,6 @@ export default function Layout() {
   }
 
   function sortStrings(strArr = []) {
-    if (strArr.length < 1) {
-      return;
-    }
-
     setSortString(strArr.slice().sort());
   }
 
